08017c7e198b924595cd2d593ddf4410dc826c16
Denis Manherz <denis@manherz.de>
2023-07-27 20:15:43 +0200

    idontknowwhatimdoin

--- flake.nix
+++ flake.nix
@@ -2,22 +2,21 @@
   description = "KDE Default Configuration";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs;
   };
 
-  outputs = { self, nixpkgs }: {
-    /*let pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
-      in {
+  outputs = { self, nixpkgs }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+    in {
       nixosConfigurations.nixos-denis = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./nixos/configuration.nix ];
       };
-    };*/
-
-    nixosConfigurations.nixos-denis = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ./nixos/configuration.nix ];
     };
 
-  };
+  /*nixosConfigurations.nixos-denis = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./nixos/configuration.nix ];
+    };*/
+
 }
