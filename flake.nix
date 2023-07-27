cfedfd488117a324e343e335e08eee2b040d1a63
Denis Manherz <denis@manherz.de>
2023-07-27 14:58:44 +0200

    With allow unfree commented

--- flake.nix
+++ flake.nix
@@ -6,6 +6,14 @@
   };
 
   outputs = { self, nixpkgs }: {
+    /*let pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+      in {
+      nixosConfigurations.nixos-denis = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./nixos/configuration.nix ];
+      };
+    };*/
+
     nixosConfigurations.nixos-denis = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./nixos/configuration.nix ];
