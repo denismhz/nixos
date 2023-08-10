427b8f2952bc249660a528080d982aa1e0c17932
Denis Manherz <denis@manherz.de>
2023-08-11 00:05:34 +0200

    sddm -theme

--- nixos/share/themes/sddm-theme.nix
+++ nixos/share/themes/sddm-theme.nix
@@ -1,16 +1,17 @@
 {pkgs}:
 
-    pkgs.stdenv.mkDerivation {
-            name = "sddm-theme";
-            src = pkgs.fetchFromGitHub {
-                    owner = "MarianArlt";
-                    repo = "sddm-sugar-dark";
-                    rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-                    sha256 = "0153z1kylbhc9d12nxy9vpn0spxgrhgy36wy37pk6ysq7akaqlvy";
-            };
-            installPhase = ''
-                    mkdir -p $out
-                    cp -R ./* $out/
-            '';
-    }
+	pkgs.stdenv.mkDerivation {
+		name = "sddm-theme";
+		src = pkgs.fetchFromGitHub {
+			owner = "denismhz";
+			repo = "sddm-sugar-dark";
+			rev = "b8ee3390bc8631ec47ea5ed1ed995bf7d9ea25d4";
+			sha256 = "sha256-ibijEqXxn6Sy6p6t+OgprWbIKg5HCWdfJk4243Tk6SI=";
+		};
+		
+		installPhase = ''
+			mkdir -p $out
+			cp -R ./* $out/
+		'';
+	}
 
