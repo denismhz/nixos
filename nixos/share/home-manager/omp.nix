2d028115614f3ff5c019a28670eb989b6fdd680c
Denis Manherz <denis@manherz.de>
2023-08-16 00:59:10 +0200

    Captured plasma config with plasma-manager
    Updated plasma config like shortcuts, windowrules, hotkeys, globals ...
    NO Theming things saved in config

--- nixos/share/home-manager/omp.nix
+++ nixos/share/home-manager/omp.nix
@@ -2,9 +2,8 @@
 {
   enable = true;
   enableBashIntegration = true;
-  #useTheme = "dracula";
   settings = builtins.fromJSON ''
-      {
+    {
       "$schema": "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/schema.json",
       "console_title_template": "{{.UserName}}@{{.HostName}} {{.Shell}}", 
       "blocks": [
@@ -32,25 +31,25 @@
               "style": "powerline",
               "type": "path"
             },
-                    {
-          "background": "#ffb86c",
-          "background_templates": [
-            "{{ if or (.Working.Changed) (.Staging.Changed) }} {{ end }}",
-            "{{ if and (gt .Ahead 0) (gt .Behind 0) }} {{ end }}",
-            "{{ if gt .Ahead 0 }} {{ end }}",
-            "{{ if gt .Behind 0 }} {{ end }}"
-          ],
-          "foreground": "#000000",
-          "powerline_symbol": "\ue0b0",
-          "properties": {
-            "branch_icon": "\ue725 ",
-            "fetch_status": true,
-            "fetch_upstream_icon": true
-          },
-          "style": "powerline",
-          "template": " {{ .UpstreamIcon }}{{ .HEAD }}{{if .BranchStatus }} {{ .BranchStatus }}{{ end }}{{ if .Working.Changed }} \uf044 {{ .Working.String }}{{ end }}{{ if and (.Working.Changed) (.Staging.Changed) }} |{{ end }}{{ if .Staging.Changed }}<#ef5350> \uf046 {{ .Staging.String }}</>{{ end }} ",
-          "type": "git"
-        },
+            {
+              "background": "#ffb86c",
+              "background_templates": [
+                "{{ if or (.Working.Changed) (.Staging.Changed) }} {{ end }}",
+                "{{ if and (gt .Ahead 0) (gt .Behind 0) }} {{ end }}",
+                "{{ if gt .Ahead 0 }} {{ end }}",
+                "{{ if gt .Behind 0 }} {{ end }}"
+              ],
+              "foreground": "#000000",
+              "powerline_symbol": "\ue0b0",
+              "properties": {
+                "branch_icon": "\ue725 ",
+                "fetch_status": true,
+                "fetch_upstream_icon": true
+              },
+              "style": "powerline",
+              "template": " {{ .UpstreamIcon }}{{ .HEAD }}{{if .BranchStatus }} {{ .BranchStatus }}{{ end }}{{ if .Working.Changed }} \uf044 {{ .Working.String }}{{ end }}{{ if and (.Working.Changed) (.Staging.Changed) }} |{{ end }}{{ if .Staging.Changed }}<#ef5350> \uf046 {{ .Staging.String }}</>{{ end }} ",
+              "type": "git"
+            },
             {
               "background": "#8be9fd",
               "foreground": "#000000",
