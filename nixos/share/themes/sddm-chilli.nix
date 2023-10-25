2feea4abd440da4c74db24e1a13911cff5ff8812
Denis Manherz <denis@manherz.de>
2023-10-25 14:55:03 +0200

    sddm-chilli

--- nixos/share/themes/sddm-chilli.nix
+++ nixos/share/themes/sddm-chilli.nix
@@ -7,7 +7,7 @@ pkgs.stdenv.mkDerivation {
     owner = "MarianArlt";
     repo = "kde-plasma-chili";
     rev = "a371123959676f608f01421398f7400a2f01ae06";
-    sha256 = "";
+    sha256 = "sha256-fWRf96CPRQ2FRkSDtD+N/baZv+HZPO48CfU5Subt854=";
   };
   
   installPhase = ''
