b94e72ff9073b9123ee5f62d8819d8bf6334ad4a
Denis Manherz <denis@manherz.de>
2023-12-20 18:19:18 +0100

    Switching to firefox

--- users/denis/home/home.nix
+++ users/denis/home/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 {
   home.username = "denis";
   home.homeDirectory = "/home/denis";
@@ -7,9 +7,8 @@
     (import ../../../modules/home-manager/alacritty.nix { inherit config pkgs; })
     (import ../../../modules/home-manager/bash.nix { inherit config pkgs; })
     (import ../../../modules/home-manager/eza.nix { inherit config pkgs; })
-    (import ../../../modules/home-manager/firefox.nix { inherit config pkgs lib; })
+    (import ../../../modules/home-manager/firefox.nix { inherit config inputs pkgs lib; })
     (import ../../../modules/home-manager/git.nix { inherit config pkgs; })
-    (import ../../../modules/home-manager/librewolf.nix { inherit config pkgs; })
     (import ../../../modules/home-manager/man.nix { inherit config pkgs; })
     (import ../../../modules/home-manager/nix-index.nix { inherit config pkgs; })
     (import ../../../modules/home-manager/oh-my-posh.nix { inherit config pkgs; })
