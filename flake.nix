9a676cc1d529e5238d0c13843ac8f50bf8ed5efb
Denis Manherz <denis@manherz.de>
2024-01-08 21:07:00 +0100

    update

--- flake.nix
+++ flake.nix
@@ -1,85 +1,103 @@
 {
   description = "KDE Default Configuration";
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-23.11;
-    unstable.url = github:nixos/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     hyprland.url = "github:hyprwm/Hyprland";
-    aithings.url = github:denismhz/flake/sd_webui;
+    aithings.url = "github:denismhz/flake/sd_webui";
+    nix-gaming.url = "github:fufexan/nix-gaming";
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = github:nix-community/home-manager/release-23.11;
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-hardware.url = "https://git.manherz.de/tobias/nixos-hardware-fork/archive/master.tar.gz";
+    neovim-flake.url = "github:jordanisaacs/neovim-flake";
   };
 
-  outputs =
-  inputs@
-  { self
-    , nixpkgs
-    , home-manager
-    , ...
-  }:
-  let
-  _users = {
-    _denis = {
-      denis.imports = [ ./users/denis/home.nix ];
-    };
-    _hypruser = {
-      hypruser.imports = [ ./users/hypruser/home.nix
-        inputs.hyprland.homeManagerModules.default];
-    };
-  };
-  in
-  {
-    # Lenovo Legion
-    nixosConfigurations.epimetheus = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./machines/epimetheus/configuration.nix
-        inputs.nixos-hardware.nixosModules.lenovo-legion-16ach6h-hybrid
-        inputs.aithings.nixosModules.invokeai-nvidia
-        inputs.aithings.nixosModules.a1111-nvidia
-        home-manager.nixosModules.home-manager
-        ({ config, ... }:
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users = nixpkgs.lib.mkMerge [ _users._denis _users._hypruser ];
-          home-manager.extraSpecialArgs = {
-            inherit inputs;
-            inherit (config.networking) hostName;
-          };
-        })
-      ];
+  outputs = inputs @ {
+    nixpkgs,
+    home-manager,
+    neovim-flake,
+    ...
+  }: let
+    system = "x86_64-linux";
+
+    configModule = {
+      config.vim.theme.name = "dracula-nvim";
     };
 
-    # ASUS Zenbook
-    nixosConfigurations.nixos-mini-denis = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.denis.imports = [
-            inputs.hyprland.homeManagerModules.default
-            ./users/denis/home.nix
-          ];
-        }
-        ./machines/iapetus/configuration.nix
-      ];
+    baseNeovim = neovim-flake.packages.${system}.maximal;
+    neovimExtended = baseNeovim.extendConfiguration {modules = [configModule];};
+
+    nvim-overlay = {
+      neovim = neovimExtended;
     };
 
-    # Rpi 3B+
-    nixosConfigurations.nixos-rpi-denis = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        ./machines/rpi/configuration.nix
-      ];
+    _users = {
+      _denis = {
+        denis.imports = [./users/denis/home.nix];
+      };
+      _hypruser = {
+        hypruser.imports = [
+          ./users/hypruser/home.nix
+          inputs.hyprland.homeManagerModules.default
+        ];
+      };
+    };
+  in {
+    nixosConfigurations = {
+      # Lenovo Legion
+      epimetheus = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./machines/epimetheus/configuration.nix
+          inputs.nixos-hardware.nixosModules.lenovo-legion-16ach6h-hybrid
+          inputs.aithings.nixosModules.invokeai-nvidia
+          inputs.aithings.nixosModules.a1111-nvidia
+          home-manager.nixosModules.home-manager
+          ({config, ...}: {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users = nixpkgs.lib.mkMerge [_users._denis _users._hypruser];
+              extraSpecialArgs = {
+                inherit inputs;
+                inherit (config.networking) hostName;
+              };
+            };
+          })
+          ({nixpkgs.overlays = [nvim-overlay];})
+        ];
+      };
+
+      # ASUS Zenbook
+      nixos-mini-denis = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.denis.imports = [
+              inputs.hyprland.homeManagerModules.default
+              ./users/denis/home.nix
+            ];
+          }
+          ./machines/iapetus/configuration.nix
+        ];
+      };
+
+      # Rpi 3B+
+      nixos-rpi-denis = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./machines/rpi/configuration.nix
+        ];
+      };
     };
   };
 }
